3905fe6bc3fe31f7d17a171d7e7f69a6a14467de
Greg Lim <limjunqu@gmail.com>
2020-04-06 11:23:27 +0800

    Update user.component.ts

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -3,6 +3,8 @@ import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firest
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 
+import { LoginService } from './login.service';	
+
 interface User{
   name: string;
   email: string;
@@ -16,11 +18,12 @@ export class UserComponent {
   usersCol: AngularFirestoreCollection<User>;  
   users: any;
 
-  constructor(private afs: AngularFirestore, private _router: Router){
+  constructor(private afs: AngularFirestore, private _router: Router,private _loginService: LoginService){
   }
 
   ngOnInit(){
-    this.usersCol = this.afs.collection('users');    
+    this.usersCol = this.afs.collection('users/' +  this._loginService.loggedInUser + '/clients');                	 
+
     this.users = this.usersCol.snapshotChanges()
     .pipe(
         map(actions => {
@@ -40,7 +43,7 @@ export class UserComponent {
 
   delete(userId,name){  
     if (confirm("Are you sure you want to delete " + name + "?")){      
-        this.afs.doc('users/'+userId).delete();  
+      this.afs.doc('users/'+ this._loginService.loggedInUser+"/clients/"+userId).delete(); 	
     }                  
   }  
 
